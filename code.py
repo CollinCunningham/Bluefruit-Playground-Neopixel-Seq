31ed0cd684d713bf30f29f6ea881cb138f2f3839
Collin Cunningham <me@collinmel.com>
2019-12-05 02:03:09 -0500

    mo formatting

--- code.py
+++ code.py
@@ -4,10 +4,7 @@ import board
 import time
 import neopixel
 
-# Works on Circuit Playground Express and Bluefruit.
-# For other boards, change board.NEOPIXEL to match the pin to which the NeoPixels are attached.
 pixel_pin = board.NEOPIXEL
-# Change to match the number of pixels you have attached to your board.
 num_pixels = 10
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
